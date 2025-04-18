74e949f4d1d254747938292c3aff0f4d8cbe2822
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-18 17:38:35 +0530

    Creating Array_List

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,1 +1,7 @@
-
+import java.util.*;
+class StudentOperations {
+ ArrayList<Student> students; // array list of data type Student (object) which is a combination of String and
+                              // int
+public StudentOperations() {
+  this.students = new ArrayList<>();
+ }
