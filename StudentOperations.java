ba66c9930e0a5bdc1b557b9ea9995ba9af868881
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-18 17:39:40 +0530

    Method to add students with exception

--- StudentOperations.java
+++ StudentOperations.java
@@ -5,3 +5,15 @@ class StudentOperations {
 public StudentOperations() {
   this.students = new ArrayList<>();
  }
+
+ // addStudents with exception
+public void addStudents(Student student) throws DuplicatePRNException, InvalidCGPAException {
+    if (searchByPRN(student.getPRN()) != null) {
+        throw new DuplicatePRNException("PRN already exists!");
+    }
+    if (student.getCGPA() < 0.0 || student.getCGPA() > 10.0) {
+        throw new InvalidCGPAException("Invalid CGPA entered! Must be between 0.0 and 10.0.");
+    }
+    students.add(student);
+    System.out.println("Student added successfully.");
+}
